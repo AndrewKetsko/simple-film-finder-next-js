b36b10d70b2d29152e4d2ee99a9c0333145af1a0
Andrew Ketsko <monicman82@gmail.com>
2023-10-14 12:18:10 +0300

    .env

--- src/getFunctions/getFunc.js
+++ src/getFunctions/getFunc.js
@@ -1,8 +1,6 @@
-const API_KEY = "cb1bcc244723619ea7f2217b5a84ccd8";
-
 export async function getTrendingData() {
   const res = await fetch(
-    `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`
   );
   if (!res.ok) return {};
   return res.json();
@@ -10,7 +8,7 @@ export async function getTrendingData() {
 
 export async function getSearchData(search) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}`
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${search}`
   );
   if (!res.ok) return {};
   return res.json();
@@ -18,7 +16,7 @@ export async function getSearchData(search) {
 
 export async function getMovieData(movieID) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieID}?api_key=${API_KEY}`
+    `https://api.themoviedb.org/3/movie/${movieID}?api_key=${process.env.API_KEY}`
   );
   if (!res.ok) return {};
   return res.json();
@@ -26,7 +24,7 @@ export async function getMovieData(movieID) {
 
 export async function getCast(movieID) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=${API_KEY}`
+    `https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=${process.env.API_KEY}`
   );
   if (!res.ok) return {};
   return res.json();
@@ -34,7 +32,7 @@ export async function getCast(movieID) {
 
 export async function getReviews(movieID) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieID}/reviews?api_key=${API_KEY}`
+    `https://api.themoviedb.org/3/movie/${movieID}/reviews?api_key=${process.env.API_KEY}`
   );
   if (!res.ok) return {};
   return res.json();
